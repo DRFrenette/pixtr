23cbdc9f7e4d39fa836006c22b010a0ff6297352
Dan Frenette <paradox815@gmail.com>
2014-09-29 15:22:42 -0400

    Add update and edit methods for galleries

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -16,6 +16,16 @@ class GalleriesController < ApplicationController
     @gallery = Gallery.find(params[:id])
   end
 
+  def update
+    @gallery = Gallery.find(params[:id])
+    @gallery.update(
+      name: params[:gallery][:name],
+      description: params[:gallery][:description]
+    )
+
+    redirect_to "/"
+  end
+  
   def create
     Gallery.create(
     name: params[:gallery][:name],
