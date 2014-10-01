db4c62e0d0b630f4539c5893739fed83f64e3b15
Dan Frenette <paradox815@gmail.com>
2014-10-01 13:49:54 -0400

    Users can edit images

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,37 @@
+class ImagesController < ApplicationController
+  def show
+    @gallery = Gallery.find(params[:gallery_id])
+    @image = @gallery.images.find(params[:id])
+  end
+
+  def new
+    @gallery = Gallery.find(params[:gallery_id])
+    @image = @gallery.images.new
+  end
+
+  def create
+    @gallery = Gallery.find(params[:gallery_id])
+    image = @gallery.images.create(image_params)
+
+    redirect_to gallery_image_path(@gallery, image)
+  end
+  
+  def edit
+    @gallery = Gallery.find(params[:gallery_id])
+    @image = @gallery.images.find(params[:id])
+  end
+
+  def update
+    @gallery = Gallery.find(params[:gallery_id])
+    @image = @gallery.images.find(params[:id])
+
+    @image.update(image_params)
+
+    redirect_to gallery_image_path(@gallery, @image)
+  end
+  private
+
+  def image_params
+    params.require(:image).permit(:name, :url)
+  end
+end
