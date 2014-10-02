846ba1c844aa5250e31616882ba612dbe77659d8
Dan Frenette <paradox815@gmail.com>
2014-10-02 14:37:50 -0400

    Associate galleries to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141001212031) do
+ActiveRecord::Schema.define(version: 20141002181651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20141001212031) do
   create_table "galleries", force: true do |t|
     t.string "name"
     t.string "description"
+    t.integer "user_id",     null: false
   end
 
   create_table "images", force: true do |t|
